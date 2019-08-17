fca564095856950dece2f468f1a410cdc289e3d9
Mr <37304121+sbondo1234@users.noreply.github.com>
2019-08-17 21:32:25 +0100

    cleanup

--- BackupFolders/FileCopyingClass.cs
+++ BackupFolders/FileCopyingClass.cs
@@ -10,6 +10,65 @@ namespace BackupFolders
 {
     public class FileCopyingClass
     {
+        public static async void StartCopying(ListBox SelectedFilesListBox, ProgressBar ProgressBar)
+        {
+            int FileCount = 0;
+
+            // Add Warning - Files Will Be Overwritten!
+
+            string[] FilePaths = new string[SelectedFilesListBox.Items.Count];
+            FilePaths = SelectedFilesListBox.Items.OfType<string>().ToArray();
+
+            MainWindow.IsDefaultBackupDirAssigned();
+
+            // Sum of how many files are going to be copied
+            foreach (string s in FilePaths)
+            {
+                FileAttributes attr = File.GetAttributes(s);
+
+                if (attr.HasFlag(FileAttributes.Directory))
+                {
+                    FileCount = Directory.GetFiles(s, "*", SearchOption.AllDirectories).Length; // FileCount = How many files in directories
+                }
+                else
+                {
+                    FileCount++; // FileCount = How many files are on their own
+                }
+                // Now, FileCount = How many files are in directories + on their own
+            }
+            ProgressBar.Maximum = FileCount;
+
+            // Organise file copying
+            foreach (string s in FilePaths)
+            {
+                FileAttributes attr = File.GetAttributes(s);
+
+                if (attr.HasFlag(FileAttributes.Directory))
+                {
+                    // Is Directory...
+                    string SourceDir = s; // Directory for source files
+                    string SourceDirFolderName = new DirectoryInfo(s).Name; // Folder source files are in
+                    MainWindow.BackupDir = $@"{Properties.Settings.Default.DefaultSaveDir}\{SourceDirFolderName}"; //Properties.Settings.Default.DefaultSaveDir
+                    bool CopySubDirs = true;
+
+                    await FileCopyingClass.DirFileBackup(ProgressBar, SourceDir, MainWindow.BackupDir, CopySubDirs);
+                }
+                else
+                {
+                    // Is File...
+                    string SourceFileDir = s; // Directory for source file
+                    string SourceFileName = Path.GetFileName(s); // Name of source file
+                    string SourceFileFolderName = Path.GetFileName(Path.GetDirectoryName(SourceFileDir)); // Folder source files are in
+                    MainWindow.BackupDir = $@"{Properties.Settings.Default.DefaultSaveDir}\{SourceFileFolderName}\{SourceFileName}";
+
+                    string DirToCreate = MainWindow.BackupDir.Replace($@"\{SourceFileName}", ""); // Remove filename from path
+                    Directory.CreateDirectory(DirToCreate); // Create directory, if needed
+
+                    await FileCopyingClass.FileBackup(ProgressBar, SourceFileDir, MainWindow.BackupDir); // Run method for file copying
+                }
+            }
+        }
+
         public static async Task FileBackup(ProgressBar ProgBar, string SourceFileDir, string BackupDir)
         {
             await Task.Run(() => File.Copy(SourceFileDir, BackupDir, true));
--- BackupFolders/MainWindow.xaml.cs
+++ BackupFolders/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace BackupFolders
     {
         string SelectedFileDir;
         string[] files;
-        public string BackupDir;
+        public static string BackupDir;
 
         // Colors
         byte ErrorA = 255;
@@ -40,7 +40,7 @@ namespace BackupFolders
 
         private void BackupWindow_Loaded(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void FileCheckBox_Click(object sender, RoutedEventArgs e)
@@ -162,7 +162,7 @@ namespace BackupFolders
             ShouldBackupFilesButtonBePlural();
         }
 
-        public string IsDefaultBackupDirAssigned()
+        public static string IsDefaultBackupDirAssigned()
         {
             if (Properties.Settings.Default.DefaultSaveDir != "")
             {
@@ -189,7 +189,7 @@ namespace BackupFolders
             return BackupDir;
         }
 
-        public string SelectBackupDir()
+        public static string SelectBackupDir()
         {
             using (var fbd = new System.Windows.Forms.FolderBrowserDialog())
             {
@@ -207,66 +207,7 @@ namespace BackupFolders
 
         private void BackupFilesButton_Click(object sender, RoutedEventArgs e)
         {
-            StartCopying();
-        }
-
-        public async void StartCopying()
-        {
-            int FileCount = 0;
-
-            // Add Warning - Files Will Be Overwritten!
-
-            string[] FilePaths = new string[SelectedFilesListBox.Items.Count];
-            FilePaths = SelectedFilesListBox.Items.OfType<string>().ToArray();
-
-            IsDefaultBackupDirAssigned();
-
-            // Sum of how many files are going to be copied
-            foreach (string s in FilePaths)
-            {
-                FileAttributes attr = File.GetAttributes(s);
-
-                if (attr.HasFlag(FileAttributes.Directory))
-                {
-                    FileCount = Directory.GetFiles(s, "*", SearchOption.AllDirectories).Length; // FileCount = How many files in directories
-                }
-                else
-                {
-                    FileCount++; // FileCount = How many files are on their own
-                }
-                // Now, FileCount = How many files are in directories + on their own
-            }
-            ProgressBar.Maximum = FileCount;
-
-            // Organise file copying
-            foreach (string s in FilePaths)
-            {
-                FileAttributes attr = File.GetAttributes(s);
-
-                if (attr.HasFlag(FileAttributes.Directory))
-                {
-                    // Is Directory...
-                    string SourceDir = s; // Directory for source files
-                    string SourceDirFolderName = new DirectoryInfo(s).Name; // Folder source files are in
-                    BackupDir = $@"{Properties.Settings.Default.DefaultSaveDir}\{SourceDirFolderName}"; //Properties.Settings.Default.DefaultSaveDir
-                    bool CopySubDirs = true;
-
-                    await FileCopyingClass.DirFileBackup(ProgressBar, SourceDir, BackupDir, CopySubDirs);
-                }
-                else
-                {
-                    // Is File...
-                    string SourceFileDir = s; // Directory for source file
-                    string SourceFileName = Path.GetFileName(s); // Name of source file
-                    string SourceFileFolderName = Path.GetFileName(Path.GetDirectoryName(SourceFileDir)); // Folder source files are in
-                    BackupDir = $@"{Properties.Settings.Default.DefaultSaveDir}\{SourceFileFolderName}\{SourceFileName}";
-
-                    string DirToCreate = BackupDir.Replace($@"\{SourceFileName}", ""); // Remove filename from path
-                    Directory.CreateDirectory(DirToCreate); // Create directory, if needed
-
-                    await FileCopyingClass.FileBackup(ProgressBar, SourceFileDir, BackupDir); // Run method for file copying
-                }
-            }
+            FileCopyingClass.StartCopying(SelectedFilesListBox, ProgressBar);
         }
     }
 }
